ebca4247662012fe3b8af953ce8e370616be7378
Sunny-Jhuang <90752081+Sunny-Jhuang@users.noreply.github.com>
2025-01-25 18:11:03 +0800

    Add files via upload

---
+++ dist/assets/BackBlog-Cd9jjQZH.js
@@ -0,0 +1,1 @@
+import{_ as s,o as c,c as t,a as r,w as n}from"./index-Ce1o5oBM.js";const d={name:"BackBlog",methods:{goBack(){this.$router.push("/blog")}}},B={class:"backblog"};function _(i,a,k,l,p,o){return c(),t("div",B,[r("h2",{onClick:a[0]||(a[0]=n((...e)=>o.goBack&&o.goBack(...e),["prevent"]))},"⬅️ 離開搜尋頁面")])}const f=s(d,[["render",_],["__scopeId","data-v-a5ad2ba3"]]);export{f as B};
---
+++ dist/assets/BlogContent-DQ6Pod7m.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o,t as n}from"./index-Ce1o5oBM.js";const s={name:"BlogContent",props:{title:{type:String,default:"Blog Title"}}},c={class:"blog-content"};function a(r,i,l,p,_,d){return e(),o("div",c,n(this.title),1)}const g=t(s,[["render",a],["__scopeId","data-v-e1d8345f"]]);export{g as B};
